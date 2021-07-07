e271d9c733ff838738d515580341b363ff871244
yadavgulshan <yadavgulshan542@gmail.com>
2021-07-07 20:09:40 +0530

    uncommented the commented part

--- modules/installer.py
+++ modules/installer.py
@@ -1,31 +1,31 @@
 from os import system
 
-# Basic softwares
-# system("dnf install code git zsh gcc curl wget vim neovim")
+# Basic softwares installation
+system("dnf install code git zsh gcc curl wget vim neovim")
 
-# # ZSH Syntax highlighting.
-# system("dnf install zsh-syntax-highlighting")
+# ZSH Syntax highlighting.
+system("dnf install zsh-syntax-highlighting")
 
-# # Gnome specific programs
-# gnome_setup = input("Do you want me to install gnome tools")
-# if(gnome_setup == "Y" or gnome_setup == "y"):
-#     system("dnf install gnome-tweak-tool")
-#     system("dnf install gnome-extensions-app")
+# Gnome specific programs
+gnome_setup = input("Do you want me to install gnome tools")
+if(gnome_setup == "Y" or gnome_setup == "y"):
+    system("dnf install gnome-tweak-tool")
+    system("dnf install gnome-extensions-app")
 
-# # Appending zsh in bashrc file
-# system("echo zsh >> ~/.bashrc")
+# Appending zsh in bashrc file
+system("echo zsh >> ~/.bashrc")
 
-# # Installing OH-MY-ZSH
-# system("""sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" """)
+# Installing OH-MY-ZSH
+system("""sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" """)
 
 # Flutter Installation
 if(input("Do you want to install Flutter?[y/N]") != "N"):
-    # print("Cloning Flutter")
-    # system("git clone https://github.com/flutter/flutter.git")
-    # system("""export PATH="$PATH:`pwd`/flutter/bin" """)
-    # system("dnf install clang cmake ninja-build pkg-config libgtk-3-dev")
-    # system("""echo export PATH="$PATH:`pwd`/flutter/bin" >> ~/.bashrc """)
-    # system("flutter precache")
+    print("Cloning Flutter")
+    system("git clone https://github.com/flutter/flutter.git")
+    system("""export PATH="$PATH:`pwd`/flutter/bin" """)
+    system("dnf install clang cmake ninja-build pkg-config libgtk-3-dev")
+    system("""echo export PATH="$PATH:`pwd`/flutter/bin" >> ~/.bashrc """)
+    system("flutter precache")
 
     # Installing Android studio.
 

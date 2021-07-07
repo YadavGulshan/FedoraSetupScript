bd0dd182734842bff9872da6f8eae95b7293c87b
yadavgulshan <yadavgulshan542@gmail.com>
2021-07-07 18:34:51 +0530

    Flatpak setup

--- modules/flatpak.py
+++ modules/flatpak.py
@@ -5,17 +5,18 @@ def initialize_print():
     print("Running your program so that it can be initialized.")
 
 
-postman = input("Do you want to install Postman?[Y/N]: ")
-if(postman == "y" or postman == "Y"):
-    print("Installing Postman using flatpak")
-    system("flatpak install flathub com.getpostman.Postman")
-    initialize_print()
-    system("flatpak run com.getpostman.Postman")
+def my_flatpak_apps():
+    postman = input("Do you want to install Postman?[Y/N]: ")
 
+    if(postman == "y" or postman == "Y"):
+        print("Installing Postman using flatpak")
+        system("flatpak install flathub com.getpostman.Postman")
+        initialize_print()
+        system("flatpak run com.getpostman.Postman")
 
-freecad = input("Do you want to install Freecad?[Y/N\: ")
+    freecad = input("Do you want to install Freecad?[Y/N]: ")
 
-if (freecad == "Y" or freecad == "y"):
-    system("flatpak install flathub org.freecadweb.FreeCAD")
-    initialize_print()
-    system("flatpak run org.freecadweb.FreeCAD")
+    if (freecad == "Y" or freecad == "y"):
+        system("flatpak install flathub org.freecadweb.FreeCAD")
+        initialize_print()
+        system("flatpak run org.freecadweb.FreeCAD")

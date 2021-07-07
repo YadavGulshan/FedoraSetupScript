e860c19cd3ea3bc5a998e30e5ae40197e06d18bc
yadavgulshan <yadavgulshan542@gmail.com>
2021-07-07 20:13:25 +0530

    Used methods for every task

--- modules/installer.py
+++ modules/installer.py
@@ -1,44 +1,49 @@
 from os import system
 
-# Basic softwares installation
-system("dnf install code git zsh gcc curl wget vim neovim")
 
-# ZSH Syntax highlighting.
-system("dnf install zsh-syntax-highlighting")
+def basic_installation():
+    # Basic softwares installation
+    system("dnf install code git zsh gcc curl wget vim neovim")
 
-# Gnome specific programs
-gnome_setup = input("Do you want me to install gnome tools")
-if(gnome_setup == "Y" or gnome_setup == "y"):
-    system("dnf install gnome-tweak-tool")
-    system("dnf install gnome-extensions-app")
+    # ZSH Syntax highlighting.
+    system("dnf install zsh-syntax-highlighting")
 
-# Appending zsh in bashrc file
-system("echo zsh >> ~/.bashrc")
+    # Gnome specific programs
+    gnome_setup = input("Do you want me to install gnome tools")
+    if(gnome_setup == "Y" or gnome_setup == "y"):
+        system("dnf install gnome-tweak-tool")
+        system("dnf install gnome-extensions-app")
 
-# Installing OH-MY-ZSH
-system("""sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" """)
+    # Appending zsh in bashrc file
+    system("echo zsh >> ~/.bashrc")
 
-# Flutter Installation
-if(input("Do you want to install Flutter?[y/N]") != "N"):
-    print("Cloning Flutter")
-    system("git clone https://github.com/flutter/flutter.git")
-    system("""export PATH="$PATH:`pwd`/flutter/bin" """)
-    system("dnf install clang cmake ninja-build pkg-config libgtk-3-dev")
-    system("""echo export PATH="$PATH:`pwd`/flutter/bin" >> ~/.bashrc """)
-    system("flutter precache")
+    # Installing OH-MY-ZSH
+    system("""sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" """)
 
-    # Installing Android studio.
 
-    # Installing requests library to make a network call to get ip addr
-    system("pip3 install requests")
-    import requests
-    try:
-        r = requests.get("http://ipconfig.me")
-        ipaddr = r.text
+def flutter():
+    # Flutter Installation
+    if(input("Do you want to install Flutter?[y/N]") != "N"):
+        print("Cloning Flutter")
+        system("git clone https://github.com/flutter/flutter.git")
+        system("""export PATH="$PATH:`pwd`/flutter/bin" """)
+        system("dnf install clang cmake ninja-build pkg-config libgtk-3-dev")
+        system("""echo export PATH="$PATH:`pwd`/flutter/bin" >> ~/.bashrc """)
+        system("flutter precache")
 
-    except:
-        print("something gone wrong!")
-    system(
-        f"wget https://r2---sn-hpjgvnj5o-cvhe.gvt1.com/edgedl/android/studio/ide-zips/4.2.2.0/android-studio-ide-202.7486908-linux.tar.gz?cms_redirect=yes&mh=Ug&mip={ipaddr}&mm=28&mn=sn-hpjgvnj5o-cvhe&ms=nvh&mt=1625660895&mv=m&mvi=2&pcm2cms=yes&pl=24&rmhost=r3---sn-hpjgvnj5o-cvhe.gvt1.com&shardbypass=yes&smhost=r1---sn-hpjgvnj5o-cvhs.gvt1.com")
-# system("")
-# system("")
+        # Installing Android studio.
+
+        # Installing requests library to make a network call to get ip addr
+        print("Installing Requests library!")
+        system("pip3 install requests")
+        import requests
+        try:
+            r = requests.get("http://ipconfig.me")
+            ipaddr = r.text
+
+        except:
+            print("something gone wrong!")
+        system(
+            f"wget https://r2---sn-hpjgvnj5o-cvhe.gvt1.com/edgedl/android/studio/ide-zips/4.2.2.0/android-studio-ide-202.7486908-linux.tar.gz?cms_redirect=yes&mh=Ug&mip={ipaddr}&mm=28&mn=sn-hpjgvnj5o-cvhe&ms=nvh&mt=1625660895&mv=m&mvi=2&pcm2cms=yes&pl=24&rmhost=r3---sn-hpjgvnj5o-cvhe.gvt1.com&shardbypass=yes&smhost=r1---sn-hpjgvnj5o-cvhs.gvt1.com")
+    # system("")
+    # system("")

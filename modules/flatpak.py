aa183f8f90364743989d2cb1832b110dc28dd550
yadavgulshan <yadavgulshan542@gmail.com>
2021-07-07 19:53:11 +0530

    Flatpak setup

--- modules/flatpak.py
+++ modules/flatpak.py
@@ -5,8 +5,18 @@ def initialize_print():
     print("Running your program so that it can be initialized.")
 
 
+def flatpak_setup():
+    flatpak = input("Do you want me to setup flatpak?[y/N]: ")
+    if (flatpak == "Y" or flatpak == "y"):
+        print("Installing flatpak")
+        system("dnf install flatpak")
+        system(
+            "flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
+        system("flatpak update")
+
+
 def my_flatpak_apps():
-    postman = input("Do you want to install Postman?[Y/N]: ")
+    postman = input("Do you want to install Postman?[y/N]: ")
 
     if(postman == "y" or postman == "Y"):
         print("Installing Postman using flatpak")
@@ -20,3 +30,6 @@ def my_flatpak_apps():
         system("flatpak install flathub org.freecadweb.FreeCAD")
         initialize_print()
         system("flatpak run org.freecadweb.FreeCAD")
+
+
+flatpak_setup()

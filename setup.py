bcb8fad3af34fbab93bdf3d674c48a1ba2ec6c01
YadavGulshan <yadavgulshan542@gmail.com>
2021-11-03 22:55:30 +0530

    added branding and did some minor changes

--- setup.py
+++ setup.py
@@ -9,7 +9,26 @@ from modules.dnf import Check_If_Fedora
 from modules.timeshift import timeshift
 from modules.installer import basic_installation, flutter, input_value, social
 
-system("echo Script Starting....")
+def Brand():
+    print("""
+  ▄████  █    ██  ██▓      ██████  ██░ ██  ▄▄▄       ███▄    █ 
+ ██▒ ▀█▒ ██  ▓██▒▓██▒    ▒██    ▒ ▓██░ ██▒▒████▄     ██ ▀█   █ 
+▒██░▄▄▄░▓██  ▒██░▒██░    ░ ▓██▄   ▒██▀▀██░▒██  ▀█▄  ▓██  ▀█ ██▒
+░▓█  ██▓▓▓█  ░██░▒██░      ▒   ██▒░▓█ ░██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒
+░▒▓███▀▒▒▒█████▓ ░██████▒▒██████▒▒░▓█▒░██▓ ▓█   ▓██▒▒██░   ▓██░
+ ░▒   ▒ ░▒▓▒ ▒ ▒ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒ 
+  ░   ░ ░░▒░ ░ ░ ░ ░ ▒  ░░ ░▒  ░ ░ ▒ ░▒░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░
+░ ░   ░  ░░░ ░ ░   ░ ░   ░  ░  ░   ░  ░░ ░  ░   ▒      ░   ░ ░ 
+      ░    ░         ░  ░      ░   ░  ░  ░      ░  ░         ░ 
+\n\n\n\n
+""")
+print('██████ Please star the repo, https://github.com/YadavGulshan/FedoraSetupScript')
+print('██████ Do check my blogs too, https://blog.BufferOverflow.me')
+print('██████ Do follow me on github if you liked this project, https://github.com/YadavGulshan')
+
+
+Brand()
+
 # os.system("NAME=Fedora")
 print("Checking if it's fedora")
 if Check_If_Fedora() != 1:
@@ -20,31 +39,31 @@ if isRoot.check_Root() == 0:
     exit()
 
 
-print("Fastest Mirrors part")
+print("Setting up the device to connect fastest mirror")
 # Changing to fastest mirror
 Check_FastestMirror()
 
 
-print("Setting max parallel download")
+print("Setting max parallel download to 10")
 # Setting max Parallel download to 10
 parallel_Download()
 
 
 # RPM fussion
-print("Enabling RPM Fusion repo")
+print("Enabling RPM Fusion")
 system("dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm")
 system("dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm")
 
 
-print("Timeshift Part")
+print("Setting up timeshift")
 # Creating a timeshift snapshot
 timeshift("Before Setup")
 
 
 
 # Copy repo to /etc/yum.repos.d for installing some programs
-print("Repo part")
-
+print("Adding required repositories to repos.d")
+print("Please remove the unused repos from /etc/yum.repos.d")
 def repo_Setup():
     pwd = getcwd()
     repo = pwd+"/repo"
